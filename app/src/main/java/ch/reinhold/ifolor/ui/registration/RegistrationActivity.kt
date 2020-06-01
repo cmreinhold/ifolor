162483625b90e23c3069f22f39a77e75071cb5ea
creinhold <christian.reinhold@gmail.com>
2020-06-01 06:49:41 +0200

    Initial commit
    Created simple project with one activity.

---
+++ app/src/main/java/ch/reinhold/ifolor/ui/registration/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package ch.reinhold.ifolor.ui.registration
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ch.reinhold.ifolor.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
